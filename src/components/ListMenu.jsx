6ba539e394cf6f461f5d367b03bcfe1469b890e5
Aurimas <vajkis1@gmail.com>
2022-11-05 19:52:16 +0200

    list menu

--- src/components/ListMenu.jsx
+++ src/components/ListMenu.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { useContext } from "react";
 import { deleteAllTasks_action, deleteCompletedTasks_action } from "../actions/taskActions";
 import DataContext from "./DataContext";
 
 function ListMenu() {
 
-    const { dispachTasks } = useContext(DataContext)
+    const { dispachTasks } = useContext(DataContext);
+
+    const [focus, setFocus] = useState(false);
 
     return (
         <div className='list-menu'>
-            <button onClick={() => dispachTasks(deleteCompletedTasks_action())}>Delete completed tasks</button>
-            <button onClick={() => dispachTasks(deleteAllTasks_action())}>Delete all tasks</button>
+            <div onClick={() => setFocus(f => !f)}>{focus ? 'Hide menu' : 'Show menu'}</div>
+            {!focus ? null :
+                <>
+                    <button onClick={() => dispachTasks(deleteCompletedTasks_action())}>Delete completed tasks</button>
+                    <button onClick={() => dispachTasks(deleteAllTasks_action())}>Delete all tasks</button>
+                </>
+            }
         </div>
-    )
+    );
 }
 
 export default ListMenu;
